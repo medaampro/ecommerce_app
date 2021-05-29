3cb5a3c038a19013b6b8c96f94cc1112fd740250
meddev <mohamedaamoum99@gmail.com>
2021-05-29 19:50:27 +0100

    design

--- client/src/Helpers/Filters.js
+++ client/src/Helpers/Filters.js
@@ -15,49 +15,53 @@ const Filters = ({filters, setFilters}) => {
     return (
         <>
             <div className="Filters">
-                <h3 onClick={ () => console.log( JSON.stringify({filters})) } >By Category :</h3>
-                <div className="by-category">
-                    { Categories.map( cat => (
-                        <div key={cat._id} >
-                            <input type="checkbox" id={cat._id} onClick = { () => {
+                <div className="first">
+                    <h3>Filter By Category :</h3>
+                    <div className="by-category">
+                        { Categories.map( cat => (
+                            <div key={cat._id} >
+                                <input type="checkbox" id={cat._id} onClick = { () => {
 
-                                if(filters.category){                                        
-                                    if( !(filters.category.includes(cat._id)) ){
-                                        setFilters({...filters, category: [...filters.category, cat._id] });
-                                    }else{
-                                        if(filters.category.length > 1){
-                                            setFilters({...filters, category: filters.category.filter( x => x !== cat._id) });
+                                    if(filters.category){                                        
+                                        if( !(filters.category.includes(cat._id)) ){
+                                            setFilters({...filters, category: [...filters.category, cat._id] });
                                         }else{
-                                            delete filters.category;
-                                            setFilters({...filters});
+                                            if(filters.category.length > 1){
+                                                setFilters({...filters, category: filters.category.filter( x => x !== cat._id) });
+                                            }else{
+                                                delete filters.category;
+                                                setFilters({...filters});
+                                            }
                                         }
+                                    }else{
+                                        setFilters({...filters, category: [cat._id]})
                                     }
-                                }else{
-                                    setFilters({...filters, category: [cat._id]})
-                                }
 
-                            } } />     
-                            <label htmlFor={cat._id}>{cat.name}</label>
-                        </div>
-                    )) }
+                                } } />     
+                                <label htmlFor={cat._id}>{cat.name}</label>
+                            </div>
+                        )) }
+                    </div>
                 </div>
-                <h3>By Price :</h3>
-                <div className="by-price">
-                    <div>
-                        <input type="radio" id="1price" name="price" onClick = { () => setFilters({...filters, price: [0, 99999] })} defaultChecked/>
-                        <label htmlFor="1price">Any</label>
-                    </div>
-                    <div>
-                        <input type="radio" id="2price" name="price" onClick = { () => setFilters({...filters, price: [0, 39] })} />
-                        <label htmlFor="2price">0$ to 39$</label>
-                    </div>
-                    <div>
-                        <input type="radio" id="3price" name="price" onClick = { () => setFilters({...filters, price: [40, 99] })} />
-                        <label htmlFor="3price">39$ to 99$</label>
-                    </div>
-                    <div>
-                        <input type="radio" id="4price" name="price" onClick = { () => setFilters({...filters, price: [100, 99999] })} />
-                        <label htmlFor="4price">More</label>
+                <div className="second">
+                    <h3>Filter By Price :</h3>
+                    <div className="by-price">
+                        <div>
+                            <input type="radio" id="1price" name="price" onClick = { () => setFilters({...filters, price: [0, 99999] })} defaultChecked/>
+                            <label htmlFor="1price">Any</label>
+                        </div>
+                        <div>
+                            <input type="radio" id="2price" name="price" onClick = { () => setFilters({...filters, price: [0, 39] })} />
+                            <label htmlFor="2price">0$ to 39$</label>
+                        </div>
+                        <div>
+                            <input type="radio" id="3price" name="price" onClick = { () => setFilters({...filters, price: [40, 99] })} />
+                            <label htmlFor="3price">39$ to 99$</label>
+                        </div>
+                        <div>
+                            <input type="radio" id="4price" name="price" onClick = { () => setFilters({...filters, price: [100, 99999] })} />
+                            <label htmlFor="4price">More</label>
+                        </div>
                     </div>
                 </div>
             </div>
