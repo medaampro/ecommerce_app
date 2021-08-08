aaeb9dd130407b382ecb25422e22c51dd1b7c79d
meddev <mohamedaamoum99@gmail.com>
2021-08-08 19:03:18 +0100

    Upload Logo

--- client/src/Helpers/config.js
+++ client/src/Helpers/config.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:5000";
-// export const API_URL = "https://meddev.herokuapp.com";
+// export const API_URL = "http://localhost:5000";
+export const API_URL = "https://meddev.herokuapp.com";
