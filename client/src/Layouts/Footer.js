759b1364160ad351a20993dd1850c090c0f04e82
meddev <mohamedaamoum99@gmail.com>
2021-08-08 21:47:36 +0100

    Last change

--- client/src/Layouts/Footer.js
+++ client/src/Layouts/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
             <footer>
                 <div className="brandInfos">
                         <Link to="/"> 
-                            <ShowImage className="logoImage" url= {`app/getLogo`} />
+                            <ShowImage className="logoImage" url= {`app/getLogo`}/>
                         </Link>
                     <p className="li-a">meddev@gmail.com</p>
                     <p className="li-a">0650335570</p>
